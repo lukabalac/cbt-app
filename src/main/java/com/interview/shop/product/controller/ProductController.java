4de3463c9d85aea06b8e05072946d930649d9c91
Luka Balac <luka@greenflamingobcn.com>
2020-07-27 22:43:49 +0200

    Implement get product method in ProductController,Add basic styling to product page

--- src/main/java/com/interview/shop/product/controller/ProductController.java
+++ src/main/java/com/interview/shop/product/controller/ProductController.java
@@ -1,4 +1,24 @@
 package com.interview.shop.product.controller;
 
+import com.interview.shop.product.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/products")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class ProductController {
+  private ProductService productService;
+
+  @GetMapping("/{id}")
+  public ModelAndView getLoginPage(@PathVariable("id") Integer id) {
+    var mav = new ModelAndView("product");
+    mav.addObject("product", productService.get(id));
+    return mav;
+  }
 }
