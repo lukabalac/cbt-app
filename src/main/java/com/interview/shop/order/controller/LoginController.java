91031d117445491932d9d4c87623bd08979e5a39
Luka Balac <luka@greenflamingobcn.com>
2020-07-27 22:31:30 +0200

    Refactor Security to match url paths more, User Entity table name, And Add Controller logic for login and access denied pages

---
+++ src/main/java/com/interview/shop/order/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.interview.shop.order.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public ModelAndView getLoginPage(){
+        var mav = new ModelAndView("login");
+        return mav;
+    }
+    @GetMapping("/access-denied")
+    public ModelAndView getEntryDeniedPage(){
+        var mav = new ModelAndView("login");
+        return mav;
+    }
+}
--- src/main/java/com/interview/shop/user/model/User.java
+++ src/main/java/com/interview/shop/user/model/User.java
@@ -14,7 +14,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
