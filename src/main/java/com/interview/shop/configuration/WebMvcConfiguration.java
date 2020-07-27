4e52b4e3706af16f9b2b5dc570e48289f9f2be76
Luka Balac <luka@greenflamingobcn.com>
2020-07-27 23:52:56 +0200

    Add popular products front end logic

--- src/main/java/com/interview/shop/configuration/WebMvcConfiguration.java
+++ src/main/java/com/interview/shop/configuration/WebMvcConfiguration.java
@@ -1,6 +1,5 @@
 package com.interview.shop.configuration;
 
-import com.interview.shop.order.OrderService;
 import com.interview.shop.product.ProductService;
 import com.interview.shop.product.model.Product;
 import com.interview.shop.user.RoleService;
@@ -8,6 +7,7 @@ import com.interview.shop.user.UserService;
 import com.interview.shop.user.model.Role;
 import com.interview.shop.user.model.RoleType;
 import com.interview.shop.user.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +18,8 @@ import java.math.BigDecimal;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
+  @Value("${spring.jpa.hibernate.ddl-auto}")
+  private String hibernateDdl;
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
     BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
@@ -26,21 +28,22 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
   @Bean
   CommandLineRunner initialSetup(
-          RoleService roleService,
-          UserService userService,
-          BCryptPasswordEncoder encoder,
-          ProductService productService) {
+      RoleService roleService,
+      UserService userService,
+      BCryptPasswordEncoder encoder,
+      ProductService productService) {
 
     System.out.println("Running initial setup ");
     return (args) -> {
-      var clientRole = roleService.create(new Role(RoleType.CLIENT));
-      var user =
-          userService.create(new User("client@foo.com", encoder.encode("client123"), clientRole));
-      var product =
-          productService.create(
-              new Product(
-                  "Kitten for sale", new BigDecimal("1000.00"), "https://picsum.photos/200"));
-
+      if (hibernateDdl.equals("create-drop") || hibernateDdl.equals("create")) {
+        var clientRole = roleService.create(new Role(RoleType.CLIENT));
+        var user =
+            userService.create(new User("client@foo.com", encoder.encode("client123"), clientRole));
+        var product =
+            productService.create(
+                new Product(
+                    "Kitten for sale", new BigDecimal("1000.00"), "https://picsum.photos/200"));
+      }
     };
   }
 }
